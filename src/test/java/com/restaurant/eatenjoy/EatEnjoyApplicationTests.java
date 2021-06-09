7bf6afc0f33ea194034e8f3af24c57cfab4a64e6
sjhello <sjhello235@gmail.com>
2021-06-09 21:17:07 +0900

    test: springboot 테스트 ignore

--- src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
+++ src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Ignore
 class EatEnjoyApplicationTests {
 
 	@Test
-	@Ignore
 	void contextLoads() {
 	}
 
