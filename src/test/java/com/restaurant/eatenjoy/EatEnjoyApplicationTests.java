2a36777d8c903a84b0d67978488c2177ee52548b
sjhello <sjhello235@gmail.com>
2021-06-09 21:39:54 +0900

    test: redis 설정 exclude

--- src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
+++ src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
@@ -2,11 +2,11 @@ package com.restaurant.eatenjoy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.CachingConfigurationSelector;
 
 @SpringBootTest(properties = {"jasypt.encryptor.password=enejaoty"})
-@EnableAutoConfiguration(exclude = RedisAutoConfiguration.class)
+@EnableAutoConfiguration(exclude = CachingConfigurationSelector.class)
 class EatEnjoyApplicationTests {
 
 	@Test
