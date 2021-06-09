5f05baaae98072ebcce7b487170cf3988cd54560
sjhello <sjhello235@gmail.com>
2021-06-09 21:09:30 +0900

    test: jenkins test

--- src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
+++ src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
@@ -3,7 +3,9 @@ package com.restaurant.eatenjoy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+	properties = {"jasypt.encryptor.password=enejaoty"}
+)
 class EatEnjoyApplicationTests {
 
 	@Test
