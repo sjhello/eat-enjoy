917d09673b0f1a910da78fb0bfa31863cc3609fe
sjhello <sjhello235@gmail.com>
2021-06-09 21:48:54 +0900

    test: ApplicationTests 클래스 Ignore

--- src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
+++ src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
@@ -1,16 +1,9 @@
 package com.restaurant.eatenjoy;
 
-import org.junit.Ignore;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cache.annotation.CachingConfigurationSelector;
-
-@Ignore
-@SpringBootTest(properties = {"jasypt.encryptor.password=enejaoty"})
+// @SpringBootTest(properties = {"jasypt.encryptor.password=enejaoty"})
 class EatEnjoyApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
