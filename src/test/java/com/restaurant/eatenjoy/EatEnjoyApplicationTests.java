5672883a1458bacb9e4fcf3e4c0f9c763e55a69a
sjhello <sjhello235@gmail.com>
2021-06-09 21:13:14 +0900

    test: jenkins test

--- src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
+++ src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
@@ -1,11 +1,11 @@
 package com.restaurant.eatenjoy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(
-	properties = {"jasypt.encryptor.password=enejaoty"}
-)
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {EatEnjoyApplicationTests.class})
 class EatEnjoyApplicationTests {
 
 	@Test
