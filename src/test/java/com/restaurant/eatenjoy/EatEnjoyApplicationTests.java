6326d7008bfeea62642deb638f605df517cf9926
sjhello <sjhello235@gmail.com>
2021-06-14 23:32:00 +0900

    test: ci feature 브랜치 테스트

--- src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
+++ src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
@@ -1,8 +1,5 @@
 package com.restaurant.eatenjoy;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 // @SpringBootTest(properties = {"jasypt.encryptor.password=enejaoty"})
 class EatEnjoyApplicationTests {
 
