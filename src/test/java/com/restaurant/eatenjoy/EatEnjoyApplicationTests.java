07fbb4288e0433ca259560e0d4a4d0967ede09eb
sjhello <sjhello235@gmail.com>
2021-06-15 23:49:21 +0900

    test: ci feature 브랜치 실패

--- src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
+++ src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
@@ -11,7 +11,7 @@ class EatEnjoyApplicationTests {
 	@Test
 	void contextLoads() {
 		System.out.println("feature/38");
-		System.out.println("werwerwer1212");
+		System.out.println("werwerwer1212333");
 	}
 
 }
