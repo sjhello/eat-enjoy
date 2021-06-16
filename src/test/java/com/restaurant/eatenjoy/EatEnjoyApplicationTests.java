fe95dfbb9cb90bfe09cd3f860a8c95b8876ce445
sjhello <sjhello235@gmail.com>
2021-06-17 00:48:15 +0900

    test: ci feature 브랜치 성공 0617

--- src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
+++ src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
@@ -6,7 +6,7 @@ class EatEnjoyApplicationTests {
 	// @Test
 	void contextLoads() {
 		System.out.println("feature/38");
-		System.out.println("asdadadada");
+		System.out.println("asdadadadaxzczx");
 	}
 
 }
